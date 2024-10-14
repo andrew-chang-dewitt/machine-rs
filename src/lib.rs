411f429d2c00b97e00eb47654e456f8089425cbf
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-10-14 09:33:46 -0500

    Change to slightly more complex example to reveal limitations.

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,85 @@
 //! A simple state machine lib
 //!
-//! # simple usage:
+//! # slightly more complex
+//!
+//! let's model a turnstile instead
 //!
 //! ```
 //! use machine::{Machine, State};
 //!
 //! #[derive(Debug, PartialEq)]
-//! enum SwitchState {
-//!   On,
-//!   Off,
+//! enum TurnstileState {
+//!   Locked,
+//!   Unlocked,
 //! }
 //!
 //! #[derive(Debug, PartialEq)]
-//! enum SwitchEvent {
-//!   Toggle,
+//! enum TurnstileEvent {
+//!   PaymentReceived,
+//!   PersonEntering,
 //! }
 //!
-//! impl State<SwitchEvent> for SwitchState {
-//!   fn apply(&self, event: SwitchEvent) -> Self {
+//! impl State<TurnstileEvent> for TurnstileState {
+//!   fn apply(&self, event: TurnstileEvent) -> Self {
 //!     match self {
-//!       Self::On => Self::Off,
-//!       Self::Off => Self::On,
+//!       Self::Locked => match event {
+//!         TurnstileEvent::PaymentReceived => Self::Unlocked,
+//!         _ => panic!("Payment required for entry.")
+//!       }
+//!       Self::Unlocked => match event {
+//!         TurnstileEvent::PersonEntering => Self::Locked,
+//!         _ => panic!("Payment already received, unable to accept payment at this time.")
+//!       }
 //!     }
 //!   }
 //! }
 //!
-//! let mut machine = Machine::new(SwitchState::Off);
-//!
+//! let mut machine = Machine::new(TurnstileState::Locked);
 //! // it starts at given state value
-//! assert_eq!(machine.state, SwitchState::Off);
-//!
+//! assert_eq!(machine.state, TurnstileState::Locked);
 //! // change state by dispatching an event
-//! machine = machine.dispatch(SwitchEvent::Toggle);
-//! assert_eq!(machine.state, SwitchState::On);
-//! machine = machine.dispatch(SwitchEvent::Toggle);
-//! assert_eq!(machine.state, SwitchState::Off);
+//! machine = machine.dispatch(TurnstileEvent::PaymentReceived);
+//! assert_eq!(machine.state, TurnstileState::Unlocked);
+//! machine = machine.dispatch(TurnstileEvent::PersonEntering);
+//! assert_eq!(machine.state, TurnstileState::Locked);
+//! ```
+//!
+//! but now, if we try to dispatch an event when the machine is in an invalid state for that
+//! event, the machine panics. this isn't ideal...
+//!
+//! ```should_panic
+//! # use machine::{Machine, State};
+//!
+//! # #[derive(Debug, PartialEq)]
+//! # enum TurnstileState {
+//! #   Locked,
+//! #   Unlocked,
+//! # }
+//!
+//! # #[derive(Debug, PartialEq)]
+//! # enum TurnstileEvent {
+//! #   PaymentReceived,
+//! #   PersonEntering,
+//! # }
+//!
+//! # impl State<TurnstileEvent> for TurnstileState {
+//! #   fn apply(&self, event: TurnstileEvent) -> Self {
+//! #     match self {
+//! #       Self::Locked => match event {
+//! #         TurnstileEvent::PaymentReceived => Self::Unlocked,
+//! #         _ => panic!("Payment required for entry.")
+//! #       }
+//! #       Self::Unlocked => match event {
+//! #         TurnstileEvent::PersonEntering => Self::Locked,
+//! #         _ => panic!("Payment already received, unable to accept payment at this time.")
+//! #       }
+//! #     }
+//! #   }
+//! # }
+//! let mut machine = Machine::new(TurnstileState::Locked);
+//! // try to enter w/out paying
+//! machine = machine.dispatch(TurnstileEvent::PersonEntering);
+//! // and it panics with "Payment required for entry"
 //! ```
 
 use std::marker::PhantomData;

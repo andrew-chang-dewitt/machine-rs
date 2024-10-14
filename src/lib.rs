576ddca700f015e28f8ba9d68feb9a3c7d08f026
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-10-14 10:25:44 -0500

    Change States to return err by convention if applying event is invalid.

--- src/lib.rs
+++ src/lib.rs
@@ -20,15 +20,15 @@
 //! }
 //!
 //! impl State<TurnstileEvent> for TurnstileState {
-//!   fn apply(&self, event: TurnstileEvent) -> Self {
+//!   fn apply(&self, event: TurnstileEvent) -> Result<Self, String> {
 //!     match self {
 //!       Self::Locked => match event {
-//!         TurnstileEvent::PaymentReceived => Self::Unlocked,
-//!         _ => panic!("Payment required for entry.")
+//!         TurnstileEvent::PaymentReceived => Ok( Self::Unlocked ),
+//!         _ => Err("Payment required for entry.".to_owned())
 //!       }
 //!       Self::Unlocked => match event {
-//!         TurnstileEvent::PersonEntering => Self::Locked,
-//!         _ => panic!("Payment already received, unable to accept payment at this time.")
+//!         TurnstileEvent::PersonEntering => Ok( Self::Locked ),
+//!         _ => Err("Payment already received, unable to accept payment at this time.".to_owned())
 //!       }
 //!     }
 //!   }
@@ -37,49 +37,26 @@
 //! let mut machine = Machine::new(TurnstileState::Locked);
 //! // it starts at given state value
 //! assert_eq!(machine.state, TurnstileState::Locked);
-//! // change state by dispatching an event
-//! machine = machine.dispatch(TurnstileEvent::PaymentReceived);
-//! assert_eq!(machine.state, TurnstileState::Unlocked);
-//! machine = machine.dispatch(TurnstileEvent::PersonEntering);
-//! assert_eq!(machine.state, TurnstileState::Locked);
-//! ```
 //!
-//! but now, if we try to dispatch an event when the machine is in an invalid state for that
-//! event, the machine panics. this isn't ideal...
+//! // and now, when we try to dispatch an event when the machine is in an invalid state for that
+//! // event, the machine instead returns an error
 //!
-//! ```should_panic
-//! # use machine::{Machine, State};
-//!
-//! # #[derive(Debug, PartialEq)]
-//! # enum TurnstileState {
-//! #   Locked,
-//! #   Unlocked,
-//! # }
-//!
-//! # #[derive(Debug, PartialEq)]
-//! # enum TurnstileEvent {
-//! #   PaymentReceived,
-//! #   PersonEntering,
-//! # }
-//!
-//! # impl State<TurnstileEvent> for TurnstileState {
-//! #   fn apply(&self, event: TurnstileEvent) -> Self {
-//! #     match self {
-//! #       Self::Locked => match event {
-//! #         TurnstileEvent::PaymentReceived => Self::Unlocked,
-//! #         _ => panic!("Payment required for entry.")
-//! #       }
-//! #       Self::Unlocked => match event {
-//! #         TurnstileEvent::PersonEntering => Self::Locked,
-//! #         _ => panic!("Payment already received, unable to accept payment at this time.")
-//! #       }
-//! #     }
-//! #   }
-//! # }
-//! let mut machine = Machine::new(TurnstileState::Locked);
 //! // try to enter w/out paying
-//! machine = machine.dispatch(TurnstileEvent::PersonEntering);
-//! // and it panics with "Payment required for entry"
+//! machine.dispatch(TurnstileEvent::PersonEntering).expect_err("Shouldn't be able to enter without paying");
+//! // turnstile remains locked
+//! assert_eq!(machine.state, TurnstileState::Locked);
+//! // so we pay as we're instructed to
+//! machine.dispatch(TurnstileEvent::PaymentReceived);
+//! assert_eq!(machine.state, TurnstileState::Unlocked); // and the turnstile unlocks
+//! // then we can enter just fine
+//! machine.dispatch(TurnstileEvent::PersonEntering); // no error => we're allowed through
+//! assert_eq!(machine.state, TurnstileState::Locked); // then state returns to locked
+//!
+//! // or if we try to pay twice, we also get a helpful error
+//! machine.dispatch(TurnstileEvent::PaymentReceived);
+//! machine.dispatch(TurnstileEvent::PaymentReceived).expect_err("Shouldn't be able to enter without paying");
+//! // turnstile remains unlocked
+//! assert_eq!(machine.state, TurnstileState::Unlocked);
 //! ```
 
 use std::marker::PhantomData;
@@ -104,14 +81,20 @@ where
         }
     }
 
-    pub fn dispatch(self, event: E) -> Self {
-        Self {
-            state: self.state.apply(event),
-            ..self
-        }
+    // switch to mutating the machine so we can recover from errors returned here by trying a
+    // different event before sending the erroneous event again.
+    // previously, the machine was consumed, meaning it was lost when an error was returned.
+    pub fn dispatch(&mut self, event: E) -> Result<(), String> {
+        self.state = self.state.apply(event)?;
+
+        // FIXME: not sure if it'd be more helpful to return a value here
+        Ok(())
     }
 }
 
 pub trait State<Event> {
-    fn apply(&self, event: Event) -> Self;
+    // FIXME: can probably do better than String as the error type
+    fn apply(&self, event: Event) -> Result<Self, String>
+    where
+        Self: Sized;
 }
